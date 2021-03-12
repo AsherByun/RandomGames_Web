df4e15e7dc5def4393a9e9a14ea277887558883a
변성재 <dfdo333@naver.com>
2021-03-12 15:13:48 +0900

    init

---
+++ src/main/java/com/games/RandomGamesWebApplication.java
@@ -0,0 +1,13 @@
+package com.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomGamesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomGamesWebApplication.class, args);
+	}
+
+}
